9a056b04f1a59c7c12fdaa58dfb2473e2b17d2c7
marc <kakudja0@gmail.com>
2025-04-21 17:57:28 +0100

    bobo

--- portfolio/src/components/footer.tsx
+++ portfolio/src/components/footer.tsx
@@ -1,5 +1,5 @@
 
-import { MessageCircle,AtSign } from "lucide-react";
+import { MessageCircle,AtSign, Linkedin } from "lucide-react";
 
 const footer = () => {
   return (
@@ -11,6 +11,7 @@ const footer = () => {
                  <div className="flex gap-4">
                  <a href="tel:+237658643770" className="hover:bg-transparent"><MessageCircle /></a> 
                  <a href="mailto:billpenka237@gmail.com" target="_blank" className="hover:bg-transparent"> <AtSign /></a>
+                 <a href="https://www.linkedin.com/in/bill-penka-260367353?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" className="hover:bg-transparent"> <Linkedin /> </a>
                  </div>
                 </div>
             </div>
