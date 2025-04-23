7766a06353b40832ecf90dd412c0d75e18919ae8
marc <kakudja0@gmail.com>
2025-04-23 07:16:14 +0100

    realese

--- portfolio/src/components/contact.tsx
+++ portfolio/src/components/contact.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
         return (
         <div className="my-[1vw]  mx-[4vw]" id="contact">
             <h2 className="text-[2rem] text-center underline md:text-[4rem] font-bold">contactez moi</h2>
-            <form action="" className="form-control md:w-[50vw] text-success items-center bg-slate-600 rounded-2xl md:mx-[15vw]  w-[80vw] space-y-6 place-items-center p-10 ">
+            <form action="https://formspree.io/f/myzwyjwb" method="POST" className="form-control md:w-[50vw] text-success items-center bg-slate-600 rounded-2xl md:mx-[15vw]  w-[80vw] space-y-6 place-items-center p-10 ">
                 <div className="form-control">
                     <label htmlFor="name" className="label">name</label><br />
                     <input type="text" name="name" placeholder="name" className=" text-black input bg-transparent input-bordered" />
@@ -19,7 +19,7 @@ const Contact = () => {
                 </div>
                 <div className="form-control">
                     <label htmlFor="message" className="label">message</label><br />    
-                    <textarea placeholder="message" className="textarea scale-[1.20] mt-3 bg-transparent textarea-bordered"></textarea>
+                    <textarea placeholder="message" name="message" className="textarea scale-[1.20] mt-3 bg-transparent textarea-bordered"></textarea>
                 </div>
                 <button type="submit" className="p-4 text-black bg-primary w-full md:w-fit rounded-xl ">Envoyer</button>
             </form>
